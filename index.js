f710c096d000e68079f07584065c2319bc5e3ac4
Nadir Alfakesh <nader.alfakesh@gmail.com>
2020-01-18 20:20:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+document.querySelector('main').remove();
+const element = document.createElement("h1");
+document.body.appendChild(element);
+element.id = "victory";
+element.innerHTML ="is the champion" ;
